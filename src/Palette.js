67d4b3b2573d222f16f2555a7025722bd78f80af
Steph <steph@Stephs-MacBook-Air.local>
2021-02-28 10:45:32 +0800

    craeted app, added palettes

---
+++ src/Palette.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Palette = () => {
+  return (
+    <div className="Palette">
+      <div className="Palette-colors"></div>
+    </div>
+  );
+};
+
+export default Palette;
